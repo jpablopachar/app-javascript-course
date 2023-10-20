f8c01be0891a1a13dacd4ac013e0f75d8d73248d
jpablopachar <jpablopachar1993@gmail.com>
2023-10-20 14:22:24 -0500

    Introducción a la consola

---
+++ js/app.js
@@ -0,0 +1,8 @@
+console.log('Hola mundo')
+console.error('Ha ocurrido un error')
+console.warn('Ten cuidado con eso')
+console.info('Información importante')
+
+document.write('Hola mundo desde el archivo app.js')
+
+// alert('Hola mundo desde el archivo app.js')
