8be04a63f159f23386bd46288ea6178062a53bbe
jpablopachar <jpablopachar1993@gmail.com>
2023-10-20 15:00:32 -0500

    Uso de variables

--- js/app.js
+++ js/app.js
@@ -1,18 +1,13 @@
-/*
-  Tipos de datos:
-  - String: "Hola mundo"
-  - Number: 123
-  - Boolean: true, false
-  - Array: ["Hola", "Mundo"]
-  - Object: { nombre: "Juan", edad: 30, isProgrammer: true }
-  - Undefined: undefined No se le ha asignado un valor
-  - Null: null Contiene un valor no válido
-*/
+// var name = 'Juan Pablo'
 
-console.log('Hola mundo')
-console.log(123)
-console.log(true, false)
-console.log(['Hola', 'Mundo'])
-console.log({ nombre: 'Juan', edad: 30, isProgrammer: true })
-console.log(undefined)
-console.log(null)
+let lastName = 'Pachar Viñan'
+
+const PI = 3.1416
+
+console.log(name, lastName, PI)
+
+// name = 'Anabel Carolina'
+
+lastName = 'Pachar Ortega'
+
+console.log(name, lastName)
