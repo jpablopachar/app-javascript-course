0171fd23958194c43690738280fb7212a4effa28
jpablopachar <jpablopachar1993@gmail.com>
2023-10-23 04:02:00 -0500

    Operaciones con booleanos

--- js/app.js
+++ js/app.js
@@ -1,30 +1,8 @@
-const number1 = 40
-const number2 = 20
-const text1 = '40'
-const text2 = 'juan'
-const text3 = 'JUAN'
-const value = null
+const value1 = true
+const value2 = false
 
-let number3
+console.log(value1 === value2)
 
-// Revisar si dos números son iguales
-console.log(number1 === number2)
+console.log(value1 ? 'Crédito aprobado' : 'Crédito no aprobado')
 
-// Revisar si dos números son diferentes
-console.log(number1 !== number2)
-
-// Revisar el tipo de dato
-
-console.log(typeof number1)
-
-console.log(typeof text1)
-
-console.log(typeof number3)
-
-console.log(typeof value)
-
-// Revisar si un string es diferente a otro
-
-console.log(text2 === text3)
-
-console.log(number1 !== text1)
+console.log(!value1 ? 'Crédito aprobado' : 'Crédito no aprobado')
