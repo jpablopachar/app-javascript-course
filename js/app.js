c674eae3c1577d7e4f1224fdeff7558862d2f80c
jpablopachar <jpablopachar1993@gmail.com>
2023-10-26 02:51:31 -0500

    Estructuras de control

--- js/app.js
+++ js/app.js
@@ -1,87 +1,52 @@
-// Declaración de función
-function sum1 () {
-  console.log(5 + 5)
+const cant = 100
+const total = 500
+const card = false
+const check = false
+const paymentMethod = 'cash'
+
+let message = ''
+
+// Comparadores === !== > < >= <= && ||
+if (cant >= total) {
+  console.log('Los fondos son suficientes')
+} else if (card) {
+  console.log('El cliente tiene tarjeta')
+} else if (check) {
+  console.log('El cliente tiene cheque')
+} else {
+  console.log('Fondos insuficientes')
 }
 
-sum1()
+// Ternario
+message = cant >= total ? 'Los fondos son suficientes' : 'Fondos insuficientes'
 
-// Declaración de función con parámetros
-function sum2 (a, b) {
-  console.log(a + b)
+console.log(message)
+
+// Switch
+switch (paymentMethod) {
+  case 'cash':
+    console.log('El cliente pagó en efectivo')
+    break
+  case 'card':
+    console.log('El cliente pagó con tarjeta')
+    break
+  case 'check':
+    console.log('El cliente pagó con cheque')
+    break
+  default:
+    console.log('No se ha utilizado un método de pago válido')
+    break
 }
 
-sum2(2, 2)
+// Literal Object
 
-// Declaración de función con parámetros y retorno
-function sum3 (a, b) {
-  return a + b
+const paymentMethods = {
+  cash: 'El cliente pagó en efectivo',
+  card: 'El cliente pagó con tarjeta',
+  check: 'El cliente pagó con cheque',
+  default: 'No se ha utilizado un método de pago válido'
 }
 
-console.log(sum3(3, 3))
+message = paymentMethods[paymentMethod] || paymentMethods.default
 
-// Expresión de función
-const sum4 = function () {
-  console.log(4 + 4)
-}
-
-sum4()
-
-// Diferencia entre una función y un método
-const number1 = '5'
-const number2 = 10
-
-console.log(parseInt(number1))
-
-console.log(number2.toString())
-
-// Comunicación entre funciones
-function start () {
-  console.log('Inicio')
-
-  middle()
-}
-
-function middle () {
-  console.log('Medio')
-
-  end()
-}
-
-function end () {
-  console.log('Final')
-}
-
-start()
-
-// Funciones en un objeto y array functions
-const myDailyRoutine = {
-  standUp: function () {
-    console.log('Levantarse')
-  },
-  selectRoutine: (routine) => {
-    switch (routine) {
-      case 1:
-        console.log('Tomar una ducha')
-        break
-      case 2:
-        console.log('Desayunar')
-        break
-      case 3:
-        console.log('Orar a Dios')
-        break
-      default:
-        console.log('Ir al trabajo')
-        break
-    }
-  },
-  returnHome: () => {
-    return 'Regresar a casa'
-  }
-}
-
-myDailyRoutine.standUp()
-myDailyRoutine.selectRoutine(3)
-myDailyRoutine.selectRoutine(2)
-myDailyRoutine.selectRoutine()
-
-console.log(myDailyRoutine.returnHome())
+console.log(message)
